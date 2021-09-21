f2e8032ce6c888a704747785526b7821dfb12d6a
LorenzoMarnat <lmarnat14@gmail.com>
2021-09-21 11:32:49 +0200

    Add modules and a switch button

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,6 +5,7 @@ const app = new Templates.AllWidget();
 const boundingVolumeBox = new THREE.Box3();
 const boundingVolumeSphere = new THREE.Sphere();
 var isReversed = false;
+var layers = {}
 
 // This method was created by iTowns
 // https://github.com/iTowns/itowns/blob/7a9457075067afa1a7aa2dc3cb72999033105ff6/src/Process/3dTilesProcessing.js#L257
@@ -60,30 +61,54 @@ const refinement = () => {
   );
 };
 
+const switchRefinement = () => {
+  if (isReversed) {
+    for (let [id, value] of Object.entries(layers)) {
+      value[0].update = refinement();
+    }
+    isReversed = false;
+  } else {
+    for (let [id, value] of Object.entries(layers)) {
+      value[0].update = reverseRefinement();
+    }
+    isReversed = true;
+  }
+};
+let btn = document.createElement('button');
+btn.innerHTML = 'Switch processing';
+document.body.appendChild(btn);
+btn.onclick = function () {
+  switchRefinement();
+};
+
 app.start('../assets/config/config.json').then(() => {
   // app.addBaseMapLayer();
   // app.addElevationLayer();
   // itowns.enableDracoLoader('./assets/draco/');
-  const layers = app.setupAndAdd3DTilesLayers();
+  layers = app.setupAndAdd3DTilesLayers();
 
   const layerChoice = new Widgets.LayerChoice(app.layerManager);
   app.addModuleView('layerChoice', layerChoice);
+
+  const debug3dTilesWindow = new Widgets.Extensions.Debug3DTilesWindow(
+    app.layerManager
+  );
+  app.addModuleView('3dtilesDebug', debug3dTilesWindow, {
+    name: '3DTiles Debug',
+  });
+
+  const cityObjectModule = new Widgets.CityObjectModule(
+    app.layerManager,
+    app.config
+  );
+  app.addModuleView('cityObjects', cityObjectModule.view);
+
   document.addEventListener(
     'keydown',
     (event) => {
       const keyName = event.key;
       if (keyName === 's') {
-        if (isReversed) {
-          for (let [id, value] of Object.entries(layers)) {
-            value[0].update = refinement();
-          }
-          isReversed = false;
-        } else {
-          for (let [id, value] of Object.entries(layers)) {
-            value[0].update = reverseRefinement();
-          }
-          isReversed = true;
-        }
+        switchRefinement();
       }
     },
     false
