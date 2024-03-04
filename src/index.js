d37200a9bebb1f62ffc1636c7ace5e704f13b68b
Lorenzo Marnat <lmarnat14@gmail.com>
2024-03-04 16:44:11 +0100

    refactor: adapt 3dtiles processing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 export { initScene, loadMultipleJSON } from '@ud-viz/utils_browser';
 export { C3DTiles } from '@ud-viz/widget_3d_tiles';
-export { RefinementButtons } from './RefinementButtons';
-export { setLayersDistanceCulling } from './3dtilesProcessing';
 export * as THREE from 'three';
 export { BaseMapLayer, C3DTilesLayer, ElevationLayer } from './layerUtils';
 export * as proj4 from 'proj4';
 export * as itowns from 'itowns';
 export { addClick3DTilesClickEvent } from './clickEvent';
+export {
+  reverseRefinement,
+  setMousePosition,
+  switchPositionReference,
+  setLayersDefaultRefinement,
+  setLayersDistanceCulling,
+} from './3dtilesProcessing';
